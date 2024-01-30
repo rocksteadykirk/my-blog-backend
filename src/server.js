3aff98353d60103e7cf639a6d60d15898c7435f0
rocksteadykirk <120696023+rocksteadykirk@users.noreply.github.com>
2024-01-31 01:24:00 +0900

    Upvoting Articles

--- src/server.js
+++ src/server.js
@@ -2,12 +2,46 @@ import express from 'express';
 
 const app = express();
 
-app.get('/hello', (req, res) => {
-    res.send('Hello');
-})
+app.use(express.json());
+
+// app.get('/hello/:name', (req, res) => {
+//     const { name } = req.params;
+//     res.send(`Hello ${name}!!`);
+// })
+
+// app.post('/hello', (req, res) => {
+//     console.log(req.body);
+//     res.send(`Hello ${req.body.name}`);
+// })
+
+let articlesInfo = [{
+    name : 'learn-react',
+    upvotes : 0,
+},
+{
+    name : 'learn-node',
+    upvotes : 0,
+},
+{
+    name : 'learn-mongodb',
+    upvotes : 0,
+}]
+
+
+
+//Upvoting articles:
+app.put('/api/articles/:name/upvote', (req, res) => {
+    const { name } = req.params;
+    const article = articlesInfo.find(a => a.name === name);
+    if (article) {
+        article.upvotes += 1;
+        res.send(`The ${name} article now has ${article.upvotes} upvotes.`)
+    } else {
+        res.send('The article doesn\'t exist.')
+    }
+});
 
 app.listen(8000, () => {
     console.log('Server is listening on port 8000')
 })
 
-//test
